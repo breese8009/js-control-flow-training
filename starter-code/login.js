2e1c44511eeeb9893424028f0fde17a66707c88b
BryanReese <bryan_reese209@yahoo.com>
2017-08-15 20:45:32 -0700

    login.js updated

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,28 @@
-console.log("login.js loaded");
+// console.log("login.js loaded");
+
+
+
+let userLogin = {
+	userName: "breese",
+	password: "bryan"
+};
+
+let entry =false;
+
+let c=0;
+
+
+while(!entry){
+	let pw=prompt("please enter your password "+userLogin.password);
+	if(userLogin.password!==pw){
+		c++;
+		alert("incorrect password");
+		if(c===3){
+			break;
+		}
+	} else if(userLogin.password===pw){
+		entry=true;
+		break;
+
+	} 
+}
